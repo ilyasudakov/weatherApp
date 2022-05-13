0f5372dff6d620fb16739a2ed5ee1ef555921981
Ilya Sudakov <ilyasudakov@inbox.ru>
2022-05-13 12:00:43 +0300

    fix cors-anywhere.herokuapp.com error

--- src/utils/API/fetchWeatherData.js
+++ src/utils/API/fetchWeatherData.js
@@ -1,24 +1,24 @@
-import { request } from './fetchTemplate'
+import { request } from "./fetchTemplate";
 
-export const getCurrentWeatherByCity = (locationData = { city: 'Moscow' }) => {
+export const getCurrentWeatherByCity = (locationData = { city: "Moscow" }) => {
   return request({
-    url: `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${locationData.city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric&lang=ru`,
-    method: 'GET',
-  })
-}
+    url: `https://cors.eu.org/api.openweathermap.org/data/2.5/weather?q=${locationData.city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric&lang=ru`,
+    method: "GET",
+  });
+};
 
 export const getCurrentWeatherByCoordinates = (
-  locationData = { lon: 0, lat: 0 },
+  locationData = { lon: 0, lat: 0 }
 ) => {
   return request({
-    url: `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?lon=${locationData.lon}&lat=${locationData.lat}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric&lang=ru`,
-    method: 'GET',
-  })
-}
+    url: `https://cors.eu.org/api.openweathermap.org/data/2.5/weather?lon=${locationData.lon}&lat=${locationData.lat}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric&lang=ru`,
+    method: "GET",
+  });
+};
 
 export const getForecastByCoordinates = (locationData = { lon: 0, lat: 0 }) => {
   return request({
-    url: `https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/onecall?lon=${locationData.lon}&lat=${locationData.lat}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&exclude=current,minutely&units=metric&lang=ru`,
-    method: 'GET',
-  })
-}
+    url: `https://cors.eu.org/api.openweathermap.org/data/2.5/onecall?lon=${locationData.lon}&lat=${locationData.lat}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&exclude=current,minutely&units=metric&lang=ru`,
+    method: "GET",
+  });
+};
